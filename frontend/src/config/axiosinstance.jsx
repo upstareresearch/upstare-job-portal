90783deb384dd1a810158423e8c652d8f22528f5
upstareresearch <yadavashok9200@gmail.com>
2026-04-15 17:20:17 +0530

    vercel file added

--- frontend/src/config/axiosinstance.jsx
+++ frontend/src/config/axiosinstance.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_BASE_URL, 
     withCredentials: true
 });
